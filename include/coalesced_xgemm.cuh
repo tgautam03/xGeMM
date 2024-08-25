beae40dd7d0bef50befd15118565d4ec34c75748
tgautam03 <30533136+tgautam03@users.noreply.github.com>
2024-08-25 16:50:14 -0600

    Removed GPU code

--- include/coalesced_xgemm.cuh
+++
@@ -1,8 +0,0 @@
-#ifndef COALESCED_XGEMM
-# define COALESCED_XGEMM
-
-void coalesced_mat_mul_kernel(float* d_A, float* d_B, float* d_C, int Nrows_A, int Nrows_B, int Ncols_B);
-
-void coalesced_xgemm(float* d_A, float* d_B, float* d_C, int Nrows_A, int Nrows_B, int Ncols_B, const int dim_block_x, const int dim_block_y);
-
-#endif
--- include/tiled_xgemm.cuh
+++
@@ -1,8 +0,0 @@
-#ifndef TILED_MAT_MUL_GPU
-# define TILED_MAT_MUL_GPU
-
-void tiled_mat_mul_kernel(float* d_A, float* d_B, float* d_C, int Nrows_A, int Nrows_B, int Ncols_B);
-
-void tiled_xgemm(float* d_A, float* d_B, float* d_C, int Nrows_A, int Nrows_B, int Ncols_B, const int dim_block_x, const int dim_block_y);
-
-#endif
