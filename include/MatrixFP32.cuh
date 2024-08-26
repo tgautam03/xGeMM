82b5e5dbd0c4c28ce1de1debef0bee7fc6e6e439
tgautam03 <30533136+tgautam03@users.noreply.github.com>
2024-08-26 09:03:48 -0600

    Added device support and modified member methods to get matrix shape

--- include/MatrixFP32.cuh
+++ include/MatrixFP32.cuh
@@ -4,19 +4,25 @@
 class MatrixFP32
 {
 private:
-    float* _mat; // Pointer to dynamic array
-    int _n_rows; // Number of rows
-    int _n_cols; // Number of cols
+    int _n_rows;        // Number of rows
+    int _n_cols;        // Number of cols
 
 public:
+    // Pointer to dynamic array
+    float* _mat;
+
+    // Matrix in device memory: true
+    bool on_device_;    
+
     // Constructor to initialize n_rows x n_cols matrix
-    MatrixFP32(int n_rows, int n_cols);
+    MatrixFP32(int n_rows, int n_cols, bool on_device);
     
     // Free memory
     void free_mat();
 
     // Member function to get matrix dimensions (n_rows, n_cols)
-    void shape(int& n_rows, int& n_cols);
+    int rows();
+    int cols();
 
     // Member Function to get value at (i,j)
     float get_val(int row, int col);
--- src/MatrixFP32.cu
+++ src/MatrixFP32.cu
@@ -1,24 +1,49 @@
+#include <assert.h>
+#include <iostream>
+
 #include "../include/MatrixFP32.cuh"
 
-MatrixFP32::MatrixFP32(int n_rows, int n_cols)
+#define CUDA_CHECK(err) {if (err != cudaSuccess){printf("%s in %s at line %d \n", cudaGetErrorString(err), __FILE__, __LINE__);exit(EXIT_FAILURE);}}
+
+MatrixFP32::MatrixFP32(int n_rows, int n_cols, bool on_device)
 {
     // Assigning Number of rows and cols to provate variables
     _n_rows = n_rows;
     _n_cols = n_cols;
 
-    // Initialize dynamic array
-    _mat = new float[_n_rows*_n_cols];
+    if (on_device == false)
+    {
+        // Initialize dynamic array
+        _mat = new float[_n_rows*_n_cols];
+        // Matrix is on host memory (RAM)
+        on_device_ = on_device;
+    }
+    else
+    {
+        // Allocate device memory
+        cudaError_t err = cudaMalloc((void**) &_mat, n_rows*n_cols*sizeof(float));
+        CUDA_CHECK(err);
+        // Matrix is on device memory (VRAM)
+        on_device_ = on_device;
+    }
 }
 
 void MatrixFP32::free_mat()
 {
-    delete[] _mat;
+    if (on_device_ == false)
+        delete[] _mat;
+    else
+        cudaFree(_mat);
 }
 
-void MatrixFP32::shape(int& n_rows, int& n_cols)
+int MatrixFP32::rows()
 {
-    n_rows = _n_rows;
-    n_cols = _n_cols;
+    return _n_rows;
+}
+
+int MatrixFP32::cols()
+{
+    return _n_cols;
 }
 
 float MatrixFP32::get_val(int row, int col)
