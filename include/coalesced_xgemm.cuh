c5f428415dbbc9282e046ec1fe337a93c8f421ed
tgautam03 <30533136+tgautam03@users.noreply.github.com>
2024-08-28 16:57:28 -0600

    Modified such that matrix dimensions are accessed using the inbuilt class methods

--- include/coalesced_xgemm.cuh
+++ include/coalesced_xgemm.cuh
@@ -3,8 +3,8 @@
 
 #include "MatrixFP32.cuh"
 
-void coalesced_mat_mul_kernel(MatrixFP32 d_A, MatrixFP32 d_B, MatrixFP32 d_C, int A_n_rows, int B_n_rows, int B_n_cols);
+void coalesced_mat_mul_kernel(MatrixFP32 d_A, MatrixFP32 d_B, MatrixFP32 d_C);
 
-void coalesced_xgemm(MatrixFP32 d_A, MatrixFP32 d_B, MatrixFP32 d_C, int A_n_rows, int B_n_rows, int B_n_cols, const int dim_block_x, const int dim_block_y);
+void coalesced_xgemm(MatrixFP32 d_A, MatrixFP32 d_B, MatrixFP32 d_C, const int dim_block_x, const int dim_block_y);
 
 #endif
